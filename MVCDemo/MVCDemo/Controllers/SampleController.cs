60c3dd4e9b1a0bbd72d05a9dad8c425a85d1cf36
Anurag Sinha <anurag3487@gmail.com>
2021-12-13 11:26:55 +0530

    MVC Learning

---
+++ MVCDemo/MVCDemo/Controllers/SampleController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class SampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Country"] = "India";
+            ViewData["Phrase"] = "HOnesty is the best policy";
+
+            ViewBag.FirstName = "Anurag";
+            ViewBag.LastName = "Sinha";
+
+            return View();
+        }
+
+
+        public IActionResult Details()
+        {
+            var model = new PersonViewModel
+            {
+                Id=101,
+                Name="John",
+                Occupation="Fireman"
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ MVCDemo/MVCDemo/Models/PersonViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCDemo.Models
+{
+    public class PersonViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Occupation { get; set; }
+    }
+}
